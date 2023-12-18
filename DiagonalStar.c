c0237c759d806e13b7bda793e8fdb3cfaba2bdda
Mithil batra <145864311+Mithilbatra@users.noreply.github.com>
2023-12-18 21:02:32 +0530

    Update DiagonalStar.c

--- DiagonalStar.c
+++ DiagonalStar.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#define RESET "\x1B[0m"
+#define RED "\x1B[31m"
+#define GREEN "\x1B[32m"
 int main() {
     int n;
     printf("Enter no of rows : ");
     scanf("%d",&n);
     int j,k,l,m;
-    printf("OUTPUT is :: \n");
+    if(n<5){
+        printf(RED "PATTERN CANNOCT BE PRINTED , INVALID NO OF ROWS\n" RESET);
+        return -1;
+    }
+    printf("OUTPUT is : \n");
     for(int k=1;k<=n;k++){
         if(k==1 || k==n){
             j=n;
@@ -13,14 +20,14 @@ int main() {
             }
             printf("\n");
         }
-        if(k>=2 && k<=(n/2)){
+        if(k>=2 && k<=(n/2) && n%2==0){
             printf("*");
             l=k-2;
             while(l--){
                 printf(" ");
             }
             printf("*");
-            m=8-(2*k);
+            m=n-(2*k);
             while(m--){
                 printf(" ");
             }
@@ -30,7 +37,7 @@ int main() {
                 printf(" ");
             }
             printf("*\n");
-        }else if(k>=((n/2)+1) && k<n){
+        }else if(k>=((n/2)+1) && k<n && n%2==0){
             printf("*");
             l=n-k-1;
             while(l-->0){
@@ -48,5 +55,44 @@ int main() {
             }
             printf("*\n");
         }
+        if(k>=2 && k<=(n/2) && n%2!=0){
+            printf("*");
+            l=k-2;
+            while(l--){
+                printf(" ");
+            }
+            printf("*");
+            m=n-(2*k);
+            while(m--){
+                printf(" ");
+            }
+            printf("*");
+            l=k-2;
+            while(l--){
+                printf(" ");
+            }
+            printf("*\n");
+        }else if(k>=((n/2)+1) && k<n && n%2!=0){
+            printf("*");
+            l=n-k-1;
+            while(l-->0){
+                printf(" ");
+            }
+            printf("*");
+            m=2*(k-((n/2)+1))-1;
+            while(m-->0){
+                printf(" ");
+            }
+            if(k!=((n/2)+1)){
+                printf("*");
+            }
+            l=n-k-1;
+            while(l-->0){
+                printf(" ");
+            }
+            printf("*\n");
+        }
     }
+    printf(GREEN "PATTERN PRINTED SUCCESSFULLY\n" RESET);
+    return 0;
 }
